9bf2c5dc8741ac424bb4eec99e2f0a0a0c47f42a
Stacy Nguyen <thaiha2905@gmail.com>
2020-04-08 03:46:26 -0400

    BST code

---
+++ w5-binary-search-trees/Sources/w5-binary-search-trees/BinarySearchTree.swift
@@ -0,0 +1,257 @@
+//
+//  BinarySearchTree.swift
+//  Binary Search Tree HW
+//
+//  Created by Stacy Nguyen on 4/7/20.
+//  Copyright © 2020 Stacy Nguyen. All rights reserved.
+//
+import Foundation
+
+class BSTNode<T: Comparable> {
+    var element: T
+    var leftChild: BSTNode?
+    var rightChild: BSTNode?
+    
+    init(element: T, leftChild: BSTNode? = nil, rightChild: BSTNode? = nil) {
+        self.element = element
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+    }
+
+    var height: Int {
+    	var count = 0
+
+    	if self.leftChild !== nil {
+	   		count += 1
+	   		count += self.leftChild!.height
+   		}
+
+    	if self.rightChild !== nil {
+   			
+   			if self.leftChild === nil {
+   				count += 1
+   				count += self.rightChild!.height
+   			}
+   			
+   			else {
+   				if self.rightChild!.height > self.leftChild!.height {
+	   				count = 1 + self.rightChild!.height
+	   			}
+   			}
+
+   		}
+
+   		return count
+    }
+
+    var size: Int {
+    	var count = 1
+    	
+    	if self.leftChild !== nil {
+    		count += self.leftChild!.size
+    	}
+
+    	if self.rightChild !== nil {
+    		count += self.rightChild!.size
+    	}
+
+    	return count
+
+    }
+
+    var elements: [T] {
+    	var elementsArray: [T] = []
+    	elementsArray.append(self.element)
+    	
+    	if self.leftChild !== nil {
+    		elementsArray = self.leftChild!.elements + elementsArray
+    	}
+
+    	if self.rightChild !== nil {
+    		elementsArray += self.rightChild!.elements
+    	}
+
+    	return elementsArray
+    }
+
+    func insert(element: T) {
+    	let newNode = BSTNode(element: element)
+
+    	if newNode.element < self.element {
+    		if self.leftChild === nil {
+    			self.leftChild = newNode
+    		}
+
+    		else {
+    			self.leftChild!.insert(element: element)
+    		}
+    	}
+
+    	if newNode.element > self.element {
+    		if self.rightChild === nil {
+    			self.rightChild = newNode
+    		}
+
+    		else {
+    			self.rightChild!.insert(element: element)
+    		}
+    	}
+
+    }
+
+    func contains(element: T) -> Bool {
+    	
+    	if self.element == element {
+    		return true
+    	}
+
+    	else {
+
+    		if self.leftChild?.contains(element: element) == true {
+    			return true
+    		}
+
+    		else if self.rightChild?.contains(element: element) == true {
+    			return true
+    		}
+
+    		else {
+    			return false
+    		}
+
+    	}
+    } 
+
+    // func delete(element: T) {
+
+    // 	if element == self.element {
+    // 		if leftChild !== nil {
+    // 			self = self.leftChild!
+    // 		}
+
+    // 		else if rightChild !== nil {
+    // 			self = self.rightChild!
+    // 		} 
+
+    // 		else {
+    // 			self = nil
+    // 		}
+    // 	}
+
+    // 	else if element < self.element {
+    // 		self.leftChild.delete(element: element)
+    // 	}
+
+    // 	else if element > self.element {
+    // 		self.rightChild.delete(element: element)
+    // 	}
+    // }
+
+}
+
+class BinarySearchTree<T: Comparable> {
+	
+	init(fromSortedData: [T] = []) {
+		
+		for x in fromSortedData {
+			self.insert(element: x)
+		}
+
+	}
+
+	var root: BSTNode<T>? = nil
+
+	var height: Int {
+
+		if self.isEmpty {
+			return 0
+		}
+
+		else {
+			return self.root!.height
+		}
+
+	}
+
+	var isEmpty: Bool {
+		return self.root === nil
+	}
+
+	var size: Int {
+		if self.isEmpty {
+			return 0
+		}
+
+		else {
+			return self.root!.size
+		}
+	}
+
+	var elements: [T] {
+		if self.isEmpty {
+			return []
+		}
+
+		else {
+			return self.root!.elements
+		}
+	}
+
+	func insert(element: T) {
+		if self.isEmpty {
+			let newNode = BSTNode(element: element)
+			self.root = newNode
+		}
+
+		else {
+			self.root!.insert(element: element)
+		}
+	}
+
+	func contains(element: T) -> Bool {
+		if self.isEmpty {
+			return false
+		}
+
+		else {
+			return self.root!.contains(element: element)
+		}
+	}
+
+	func search(element: T) -> T? {
+		if self.contains(element: element) {
+			return element
+		}
+
+		else {
+			return nil
+		}
+	}
+
+	func makeBreadthFirstArray() -> [T] {
+		var makeBreadthFirstArray: [T] = []
+		var queue: [BSTNode<T>] = []
+		var currentNode = self.root
+
+		if !self.isEmpty {
+			queue.append(currentNode!)
+		}
+
+		while !queue.isEmpty {
+			
+			currentNode = queue.removeFirst()
+			makeBreadthFirstArray.append(currentNode!.element)
+			
+			if currentNode!.leftChild !== nil {
+				queue.append(currentNode!.leftChild!)
+			}
+
+			if currentNode!.rightChild !== nil {
+				queue.append(currentNode!.rightChild!)
+			}
+		}
+
+		return makeBreadthFirstArray
+	}
+
+}
--- w5-binary-search-trees/Sources/w5-binary-search-trees/main.swift
+++ w5-binary-search-trees/Sources/w5-binary-search-trees/main.swift
@@ -1,1 +1,19 @@
-print("Hello, world!")
+//
+//  main.swift
+//  Binary Search Tree HW
+//
+//  Created by Stacy Nguyen on 4/7/20.
+//  Copyright © 2020 Stacy Nguyen. All rights reserved.
+//
+import Foundation
+
+func main() {
+	let BST = BinarySearchTree<Int>(fromSortedData: [16, 11, 20, 1, 15, 31, 12])
+
+	print(BST.elements)
+	print("height: \(BST.height)")
+	print("size: \(BST.size)")
+	print(BST.makeBreadthFirstArray())
+}
+
+main()
